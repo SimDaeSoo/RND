3ded1d874306f467337b6179f045f7fd7ac7d8e9
bigdeal <bigdeal@smartstudy.co.kr>
2020-08-06 19:44:37 +0900

    console.log change.

--- test/src/index.ts
+++ test/src/index.ts
@@ -5,20 +5,20 @@ async function main(): Promise<void> {
     await slave.open();
     slave.connectMaster('http://localhost:1000');
     slave.masterSocket.on('connect', (): void => {
-        console.log('connected master server');
+        console.log(`[${new Date().toISOString()}] success connect to master server`);
     });
+
     const slave_2 = new Server();
     slave_2.connectMaster('http://localhost:1000');
     slave_2.masterSocket.on('connect', (): void => {
-        console.log('connected master server');
+        console.log(`[${new Date().toISOString()}] success connect to master server`);
     });
 
     const connectionServer = new Server({ port: 1000 });
     await connectionServer.open();
     connectionServer.socketServer.on('connection', (socket): void => {
-        const remoteAddress = socket.conn.remoteAddress;
-        console.log(remoteAddress);
-        console.log('slave server is connected');
+        const remoteAddress = socket.conn.remoteAddress.replace(/::ffff:/g, '');
+        console.log(`[${new Date().toISOString()}] slave server[${remoteAddress}] connected`);
     });
 }
 
